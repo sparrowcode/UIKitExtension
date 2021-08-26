70592e96f6f5017d588975634f8dadaea5bac2e1
Ivan Vorobei <hello@ivanvorobei.io>
2021-08-26 21:51:34 +0300

    Fixed compatibility.

--- Package.swift
+++ Package.swift
@@ -9,20 +9,19 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "NativeUIKit",
-            targets: ["NativeUIKit"]
+            name: "NativeUIKit", targets: ["NativeUIKit"]
         )
     ],
     dependencies: [
-        .package(name: "SparrowKit", url: "https://github.com/ivanvorobei/SparrowKit", .upToNextMajor(from: "3.2.2")),
-        .package(name: "SPPerspective", url: "https://github.com/ivanvorobei/SPPerspective", .upToNextMajor(from: "1.3.5")),
+        .package(name: "SparrowKit", url: "https://github.com/ivanvorobei/SparrowKit", .upToNextMajor(from: "3.2.5")),
+        .package(name: "SPPerspective", url: "https://github.com/ivanvorobei/SPPerspective", .upToNextMajor(from: "1.3.6"))
     ],
     targets: [
         .target(
             name: "NativeUIKit",
             dependencies: [
                 .product(name: "SparrowKit", package: "SparrowKit"),
-                    .product(name: "SPPerspective", package: "SPPerspective")
+                .product(name: "SPPerspective", package: "SPPerspective")
             ],
             swiftSettings: [
                 .define("NATIVEUIKIT_SPM")
