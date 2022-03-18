cefa01c16cd097048f724ac240a6d14b793d9edf
Ivan Vorobei <hello@ivanvorobei.io>
2022-03-18 14:30:44 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ivanvorobei/SparrowKit", .upToNextMajor(from: "3.5.8")),
+        .package(url: "https://github.com/ivanvorobei/SparrowKit", .upToNextMajor(from: "3.6.0")),
         .package(url: "https://github.com/ivanvorobei/SPPerspective", .upToNextMajor(from: "1.4.1")),
-        .package(url: "https://github.com/ivanvorobei/SPDiffable", .upToNextMajor(from: "4.0.9")),
+        .package(url: "https://github.com/ivanvorobei/SPDiffable", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/ivanvorobei/SPPageController", .upToNextMajor(from: "1.3.2"))
     ],
     targets: [
